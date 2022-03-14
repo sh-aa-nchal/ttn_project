59f1ccbaeae6d062fb80ec3e85c1a0d3fedd2ec5
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 02:07:08 +0530

    Create tomcat.sh
    Tomcat installation

---
+++ modules/asg/tomcat.sh
@@ -0,0 +1,52 @@
+#!bin/bash
+
+sudo apt-get update -y
+sudo apt-get install default-jdk -y
+
+
+##Installing tomcat
+sudo groupadd tomcat
+sudo useradd -g tomcat -d /opt/tomcat tomcat
+cd /tmp
+curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.59/bin/apache-tomcat-9.0.59.tar.gz
+sudo mkdir /opt/tomcat
+sudo tar xzvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
+cd /opt/tomcat
+sudo chown -RH tomcat: /opt/tomcat
+sudo chmod -R g+r conf
+sudo chmod g+x conf
+sudo chown -R tomcat webapps/ work/ temp/ logs/
+
+##creating syatemd file
+sudo touch tomcat.service
+    sudo chmod 777 tomcat.service
+    echo "[Unit]" > tomcat.service
+    echo "Description=Apache Tomcat Web Application Container" >> tomcat.service
+    echo "After=network.target" >> tomcat.service
+
+    echo "[Service]" >> tomcat.service
+    echo "Type=forking" >> tomcat.service
+
+    echo "Environment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64" >> tomcat.service
+    echo "Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid" >> tomcat.service
+    echo "Environment=CATALINA_HOME=/opt/tomcat" >> tomcat.service
+    echo "Environment=CATALINA_BASE=/opt/tomcat" >> tomcat.service
+    echo "Environment=CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC" >> tomcat.service
+    echo "Environment=JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom" >> tomcat.service
+
+    echo "ExecStart=/opt/tomcat/bin/startup.sh" >> tomcat.service
+    echo "ExecStop=/opt/tomcat/bin/shutdown.sh" >> tomcat.service
+
+    echo "User=tomcat" >> tomcat.service
+    echo "Group=tomcat" >> tomcat.service
+    echo "UMask=0007" >> tomcat.service
+    echo "RestartSec=10" >> tomcat.service
+    echo "Restart=always" >> tomcat.service
+
+    echo "[Install]" >> tomcat.service
+    echo "WantedBy=multi-user.target" >> tomcat.service
+
+    sudo mv tomcat.service /etc/systemd/system/tomcat.service
+    sudo chmod 755 /etc/systemd/system/tomcat.service
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
