d944b87c12a8831b09970da5c8d6e1ee0f916384
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 13:58:38 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+##Network
+variable "vpc_cdr" {}
+
+variable "public1_cdr" {}
+
+variable "public2_cdr" {}
+
+variable "private_cdr" {}
+
+#3asg
+variable "ami-ins" {}
+variable "ins-type" {}
+variable "desired-cap" {}
+variable "max-count" {}
+variable "min-count" {}
+variable "min-healthy" {}
+
+##database
+variable "db-ami" {}
+variable "db-instance" {}
+
+##monitoring
+variable "mon-ami" {}
+variable "instance-mon" {}
