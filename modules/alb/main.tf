c2abdb1f56e921365138042bca9973eeaa3fc539
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 12:42:08 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,15 +1,8 @@
-resource "aws_default_vpc" "default" {
-    tags = {
-        Name = "Default VPC"
-    }
-  
-}
-
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = []
+  security_groups    = [var.sec_group_id]
   subnets            = [var.subnet1, var.subnet2]
 
   enable_deletion_protection = true
@@ -20,7 +13,42 @@ resource "aws_lb" "test" {
   }
 }
 
-resource "aws_placement_group" "test" {
-  name     = "test"
-  strategy = "cluster"
+resource "aws_security_group" "public-sg" {
+    name   = "public-sg"
+    vpc_id = aws_vpc.Main.id
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = var.test-ip
+    }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = var.test-ip
+    }
+    ingress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = [aws_vpc.Main.cidr_block]
+    }
+  ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = var.test-ip
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+resource "aws_security_group" "private-sg" {
+ name = "private-sg"
+ vpc_id = aws_vpc.Main.id
 }
