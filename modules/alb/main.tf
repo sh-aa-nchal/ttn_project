9702d94e245e1982a157ab658c629b6688e2e40a
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 16:36:30 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -51,4 +51,18 @@ resource "aws_security_group" "public-sg" {
 resource "aws_security_group" "private-sg" {
  name = "private-sg"
  vpc_id = aws_vpc.Main.id
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+     }
+     egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
