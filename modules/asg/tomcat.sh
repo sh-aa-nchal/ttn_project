7e76275c10edb2fe20de253413b9a3abb873978f
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 16:18:12 +0530

    Update tomcat.sh

--- modules/asg/tomcat.sh
+++ modules/asg/tomcat.sh
@@ -54,4 +54,56 @@ sudo systemctl start tomcat
 ##maven application
 git clone https://github.com/khoubyari/spring-boot-rest-example.git
 sudo apt install maven -y
+mvn clean package
+
+
+##telegraf installation and configuration
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt update
+sudo apt install telegraf
+sudo systemctl enable --now telegraf
+
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt update
+sudo apt install telegraf
+sudo systemctl enable --now telegraf
+#sudo mv /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf.backup
+cd /etc
+sudo chown -R ubuntu:ubuntu telegraf
+cd telegraf
+touch telegraf.conf
+echo "[global_tags]" > telegraf.conf
+echo "[agent]" >> telegraf.conf
+echo '  interval = "10s"'>> telegraf.conf
+echo "  round_interval = true" >> telegraf.conf
+echo "  metric_batch_size = 1000" >> telegraf.conf
+echo "  metric_buffer_limit = 10000 " >> telegraf.conf
+echo '  collection_jitter = "0s"' >> telegraf.conf
+echo '  flush_interval = "10s"' >> telegraf.conf
+echo '  flush_jitter = "10s"' >> telegraf.conf
+echo '  precision = ""' >> telegraf.conf
+echo '  hostname = ""' >> telegraf.conf
+echo '  omit_hostname = "false"' >> telegraf.conf
+echo "[[outputs.influxdb]]" >> telegraf.conf
+echo '  urls = ["http://  :8086"]' >> telegraf.conf
+echo '  database = "telegraf"' >> telegraf.conf
+echo '  username = "telegraf"' >> telegraf.conf
+echo '  password = "influxpass"' >> telegraf.conf
+echo "[[inputs.tomcat]]" >> telegraf.conf
+echo '  url = "http://127.0.0.1:8080/manager/status/all?XML=true"' >> telegraf.conf
+echo '  username = "tomcat"' >> telegraf.conf
+echo '  password = "password"' >> telegraf.conf
+echo "[[inputs.system]]" >> telegraf.conf
+echo "[[inputs.cpu]]" >> telegraf.conf
+echo "  percpu = true" >> telegraf.conf
+echo "  totalcpu = true" >> telegraf.conf
+echo "  collect_cpu_time = false" >> telegraf.conf
+echo "  report_active = false" >> telegraf.conf
+
+sudo systemctl restart telegraf
+
 
