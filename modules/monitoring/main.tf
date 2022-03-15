1fd624f228e82a2557663c358b5f9544f384bdbb
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 13:16:34 +0530

    Update main.tf

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -9,6 +9,8 @@ resource "aws_instance" "database-instance" {
   ami           = var.ami-mon
   instance_type = var.instance-mon
   key_name = data.aws_key_pair.pro-key.key_name
+  subnet_id = var.subnet-mon
+  vpc_security_group_ids = [var.sg-mon]
   user_data = filebase64("${path.module}/userdata.sh")
   tags = {
 		Name = "monitoring-test"
