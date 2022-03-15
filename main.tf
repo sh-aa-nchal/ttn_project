a11ca6e6cb068b9f5292b51977429685dae54452
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 11:10:10 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+module "network" {
+  source = "./modules/network"
+  main_vpc_cidr =
+  public_subnet1 =
+  public_subnet2 = 
+}
+  
+module "alb" {
+  source  = "./modules/alb"
+  
+}
+  
+module "asg" {
+  source = "./modules/asg"
+  ami_id =
+  instance_type = 
+  desired_cap =
+  max_count =
+  min_count =
+  subnet1_id =
+  subnet2_id =
+  min_healthy =
+  
+}
+  
+module "database" {
+  source = "./modules/database"
+  
+}
+  
+module "monitoring" {
+  source = "./modules/monitoring"
+}
+  
