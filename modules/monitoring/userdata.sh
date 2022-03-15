1a9c2db3acbfbae2c385aabae0bc6757c5fa5487
vagishaj <99390379+vagishaj@users.noreply.github.com>
2022-03-15 13:01:50 +0530

    Update userdata.sh

--- modules/monitoring/userdata.sh
+++ modules/monitoring/userdata.sh
@@ -3,7 +3,7 @@
 #elasticsearch
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt update
+sudo apt update -y
 sudo apt install elasticsearch
 sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch.service
@@ -15,13 +15,13 @@ sudo chown -R root:elasticsearch elasticsearch
 #installing influxdb
 echo "deb https://repos.influxdata.com/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install influxdb
 sudo systemctl enable --now influxdb
 sudo systemctl start influxdb
 
 #grafana
-sudo apt update
+sudo apt update -y
 sudo apt-get install -y gnupg2 curl software-properties-common
 curl https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
@@ -31,6 +31,6 @@ sudo systemctl enable --now grafana-server
 
 #kibana
 echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.5.x.list
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install kibana
 
