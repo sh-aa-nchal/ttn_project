ad19f994394963bac881c676c18953e8c2179312
AkashShuklaTTN <99382670+AkashShuklaTTN@users.noreply.github.com>
2022-03-15 10:26:28 +0530

    Add files via upload

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,1 +1,26 @@
-#alb
+resource "aws_default_vpc" "default" {
+    tags = {
+        Name = "Default VPC"
+    }
+  
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = []
+  subnets            = [var.subnet1, var.subnet2]
+
+  enable_deletion_protection = true
+
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_placement_group" "test" {
+  name     = "test"
+  strategy = "cluster"
+}
