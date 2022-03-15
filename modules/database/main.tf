a152d6506fbba9bf834680f8eb0410ecb0cf040b
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 16:24:42 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "database-instance" {
   ami           = var.ami-db
   instance_type = var.instance-db
   key_name = data.aws_key_pair.pro-key.key_name
+  private_ip = variable.privateip-db
   subnet_id = var.subnet-db
   vpc_security_group_ids = [var.sg-db]
   user_data = filebase64("${path.module}/userdata.sh")
