bc90d336ff3c689d0e522ff96667e98ed63233aa
AkashShuklaTTN <99382670+AkashShuklaTTN@users.noreply.github.com>
2022-03-15 10:51:48 +0530

    Add files via upload

---
+++ modules/terraform-vpc/securitygroup.tf
@@ -0,0 +1,47 @@
+module "public_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-01ff7b24ff228be2e"
+
+  ingress_cidr_blocks      = ["10.0.0.64/26", "10.0.0.128/26"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "http-80-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
+module "private_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-01ff7b24ff228be2e"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "http-80-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
---
+++ modules/terraform-vpc/variables.tf
@@ -0,0 +1,4 @@
+ variable "main_vpc_cidr" {}
+ variable "public_subnet1" {}
+ variable "public_subnet2" {}
+ variable "private_subnets" {}
