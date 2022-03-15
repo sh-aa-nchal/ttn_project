ed6a70ad7d7771956b7c0ce99ce6924344ff8fcc
sh-aa-nchal <99381803+sh-aa-nchal@users.noreply.github.com>
2022-03-15 16:39:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-backend "s3" {
-    bucket         = "ter-backend-bucket"
-    key            = "ter-backend-bucket/terraform.tfstate"
-    encrypt        = true
-    region         = "us-east-1"
-    dynamodb_table = "ter-dyanamo-locking"
-  }
-}
+##terraform {
+#3backend "s3" {
+  ##  bucket         = "ter-backend-bucket"
+   ## key            = "ter-backend-bucket/terraform.tfstate"
+   ## encrypt        = true
+   ## region         = "us-east-1"
+    ##dynamodb_table = "ter-dyanamo-locking"
+ ## }
+##}
