1c2d152bf761541be2b5371ffcb5f3f766d648ce
vagishaj <99390379+vagishaj@users.noreply.github.com>
2022-03-15 16:19:21 +0530

    Update main.tf

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -5,12 +5,13 @@ data "aws_key_pair" "pro-key" {
     values = ["project"]
   }
 
-resource "aws_instance" "database-instance" {
+resource "aws_instance" "monitoring-instance" {
   ami           = var.ami-mon
   instance_type = var.instance-mon
   key_name = data.aws_key_pair.pro-key.key_name
   subnet_id = var.subnet-mon
   vpc_security_group_ids = [var.sg-mon]
+  private_ip= var.privateip-mon
   user_data = filebase64("${path.module}/userdata.sh")
   tags = {
 		Name = "monitoring-test"
